a495513bbe64b2b6e735ad1c4a6155e12a4143cf
YoanRamirez <yoan.ramirezb@gmail.com>
2021-08-31 15:25:06 -0500

    comit30.2

--- bd-yoan/bd-yoan.php
+++ bd-yoan/bd-yoan.php
@@ -1,53 +1,9 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <input type="text" name="texto">
-
-    <table>
-    <tr>
-        <td></td>
-        <td>Lunes</td>
-        <td>Martes</td>
-        <td>Miércoles</td>
-        <td>Jueves</td>
-        <td>Viernes</td>
-    </tr>
-    <tr>
-        <td>10:30</td>
-        <td>Matemáticas</td>
-        <td>Geografía</td>
-        <td>Física</td>
-        <td>Dibujo</td>
-        <td>Matemáticas</td>
-    </tr>
-    <tr>
-        <td>11:30</td>
-        <td>Inglés</td>
-        <td>Lenguaje</td>
-        <td>Geografía</td>
-        <td>Química</td>
-        <td>Física</td>
-    </tr>
-</table>
-</body>
-
-</html> -->
-
-
 <?php
 //conectar a bd               host    ,usuario,pass,name bd
 $conexion = mysqli_connect("localhost", "root", "", "bd_yoantest");
 //test conection
 if (mysqli_connect_errno()) {
-    echo "fallamos al conectarnos " . mysqli_connect_error();
+    echo "fallamos al conectar nos " . mysqli_connect_error();
 } else {
     echo "bien conectado <br>  ";
 }
@@ -63,5 +19,48 @@ $query = mysqli_query($conexion, "SELECT * FROM usuario");
 //recorrer la tabla de la bd
 while ($usuario =  mysqli_fetch_assoc($query)) {
     echo "<br>";
-    var_dump($usuario);
+    // var_dump($usuario);
+    echo $usuario['nombre'] . "<br>";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 align="center">Info del form</h1>
+    <table width="70%" border="1px" align="center">
+        <tr align="center">
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Edad</td>
+            <td>Rut</td>
+            <td>Telefono</td>
+        </tr>
+        <?php
+     
+        $query = mysqli_query($conexion, "SELECT * FROM usuario");
+        while ($usuario =  mysqli_fetch_assoc($query)) {
+            echo "<br>";          
+        ?>
+            <tr>
+                <td><?php echo  $usuario['nombre']  ?></td>
+                <td><?= $usuario['apellido'] ?></td>
+                <td><?= $usuario['edad']  ?></td>
+                <td><?= $usuario['rut'] ?></td>
+                <td><?= $usuario['telefono']  ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+</body>
+
+</html>
